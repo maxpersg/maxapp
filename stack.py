dabc8a27406087fa4fe5877d15191a88db613d08
Max Eng <maxpersg@gmail.com>
2015-05-10 21:13:25 +0800

    bug fixes

--- stack.py
+++ stack.py
@@ -1,18 +1,19 @@
 from yowsup.stacks import YowStack
 from .layer import EchoLayer
 from yowsup.layers import YowLayerEvent
-from yowsup.layers.auth                        import YowCryptLayer, YowAuthenticationProtocolLayer, AuthError
-from yowsup.layers.coder                       import YowCoderLayer
-from yowsup.layers.network                     import YowNetworkLayer
-from yowsup.layers.protocol_messages           import YowMessagesProtocolLayer
-from yowsup.layers.protocol_media              import YowMediaProtocolLayer
-from yowsup.layers.stanzaregulator             import YowStanzaRegulator
-from yowsup.layers.protocol_receipts           import YowReceiptProtocolLayer
-from yowsup.layers.protocol_acks               import YowAckProtocolLayer
-from yowsup.layers.logger                      import YowLoggerLayer
-from yowsup.layers.axolotl                     import YowAxolotlLayer
-from yowsup.layers.protocol_iq                 import YowIqProtocolLayer
-from yowsup.layers.protocol_calls              import YowCallsProtocolLayer
+from yowsup.layers.auth                         import YowCryptLayer, YowAuthenticationProtocolLayer, AuthError
+from yowsup.layers.coder                        import YowCoderLayer
+from yowsup.layers.network                      import YowNetworkLayer
+from yowsup.layers.protocol_messages            import YowMessagesProtocolLayer
+from yowsup.layers.protocol_media               import YowMediaProtocolLayer
+from yowsup.layers.stanzaregulator              import YowStanzaRegulator
+from yowsup.layers.protocol_receipts            import YowReceiptProtocolLayer
+from yowsup.layers.protocol_acks                import YowAckProtocolLayer
+from yowsup.layers.logger                       import YowLoggerLayer
+from yowsup.layers.axolotl                      import YowAxolotlLayer
+from yowsup.layers.protocol_iq                  import YowIqProtocolLayer
+from yowsup.layers.protocol_iq.protocolentities import *
+from yowsup.layers.protocol_calls               import YowCallsProtocolLayer
 from yowsup.common import YowConstants
 from yowsup import env
 
