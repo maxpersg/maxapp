dc4b9f39fa81eba2c7d94f5cd568037e0526a13d
Max Eng <maxpersg@gmail.com>
2015-05-13 22:58:31 +0800

    bug fixes

--- layer.py
+++ layer.py
@@ -76,7 +76,7 @@ class EchoLayer(YowInterfaceLayer):
         elif messagereceived == "bustotown":
             results = ltadatamall.bus()
 
-            self.ReplyWith(results[2][1],sendto)
+            self.ReplyWith(results["Services"][1],sendto)
             #self.ReplyWith(results["Services"]["SubsequentBus"],sendto)
 
 
