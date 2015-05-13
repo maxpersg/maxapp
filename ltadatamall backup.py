7ac0d84e5f3b3ae654488eb477a52a991071684d
Max Eng <maxpersg@gmail.com>
2015-05-13 22:21:40 +0800

    bug fixes

---
+++ ltadatamall backup.py
@@ -0,0 +1,53 @@
+import json
+import urllib
+from urlparse import urlparse
+import httplib2 as http
+
+#External library
+def traffic():
+
+        #Authentication parameter
+        headers = { 'AccountKey' : 'MnvwjNVGDVKl8Ig9uipL3Q==',
+                'UniqueUserID' : 'ee84f4e2-caae-4ab5-afa3-02a7348efd2f',
+                'accept' : 'application/json'} #Request results in JSON
+
+        #API parameters
+        uri = 'http://datamall.cloudapp.net' #Resource URL
+        path = '/ltaodataservice.svc/IncidentSet?'
+
+        #Query parameters
+        params = {}#'Latitude':'1.304980', #Search within a radius
+        #       'Longitude':'103.831984', # from a central point
+        #       'Distance':'5000'}; # Distance in metres
+
+        #Build query string & specify type of API call
+        target = urlparse(uri + path + urllib.urlencode( params ) )
+        print target.geturl()
+        method = 'GET'
+        body = ''
+
+        #Get handle to http
+        h = http.Http()
+
+        #Obtain results
+        response, content = h.request(
+                target.geturl(),
+                method,
+                body,
+                headers)
+        #Parse JSON to print
+        jsonObj = json.loads(content)
+        #print json.dumps(jsonObj, sort_keys=True, indent=4)
+
+        #print len(jsonObj["d"])
+
+        #for x in jsonObj["d"]:
+                #print(jsonObj["d"][str(x)]["Message"])
+        #        print x["Message"]
+        #print json.dump(
+        #print content
+
+        #Save result to file
+        #with open("traffic_incidents.json","w") as outfile: #Saving jsonObj["d"]
+        #        json.dump(jsonObj, outfile, sort_keys=True, indent=4, ensure_ascii=False)
+        return jsonObj
---
+++ ltadatamall.py
@@ -0,0 +1,76 @@
+import json
+import urllib
+from urlparse import urlparse
+import httplib2 as http
+
+#External library
+
+        #Authentication parameter
+        headers = { 'AccountKey' : 'MnvwjNVGDVKl8Ig9uipL3Q==',
+                'UniqueUserID' : 'ee84f4e2-caae-4ab5-afa3-02a7348efd2f',
+                'accept' : 'application/json'} #Request results in JSON
+
+        #API parameters
+        uri = 'http://datamall.cloudapp.net' #Resource URL
+
+
+
+def traffic():
+        path = '/ltaodataservice.svc/IncidentSet?'
+        #Query parameters
+        params = {}#'Latitude':'1.304980', #Search within a radius
+        #       'Longitude':'103.831984', # from a central point
+        #       'Distance':'5000'}; # Distance in metres
+
+        #Build query string & specify type of API call
+        target = urlparse(uri + path + urllib.urlencode( params ) )
+        print target.geturl()
+        method = 'GET'
+        body = ''
+
+        #Get handle to http
+        h = http.Http()
+
+        #Obtain results
+        response, content = h.request(
+                target.geturl(),
+                method,
+                body,
+                headers)
+
+        #Parse JSON to print
+        jsonObj = json.loads(content)
+
+        return jsonObj
+
+def bus():
+        path = '/ltaodataservice/BusArrival?'
+
+        #Query parameters
+        params = {'BusStopID':'44961', #bef blk 484
+                'ServiceNo':'922'}
+
+        #'Latitude':'1.304980', #Search within a radius
+        #       'Longitude':'103.831984', # from a central point
+        #       'Distance':'5000'}; # Distance in metres
+
+        #Build query string & specify type of API call
+        target = urlparse(uri + path + urllib.urlencode( params ) )
+        print target.geturl()
+        method = 'GET'
+        body = ''
+
+        #Get handle to http
+        h = http.Http()
+
+        #Obtain results
+        response, content = h.request(
+                target.geturl(),
+                method,
+                body,
+                headers)
+        
+        #Parse JSON to print
+        jsonObj = json.loads(content)
+
+        return jsonObj
