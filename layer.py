bb9e48cf5b7f1abd5b0a2a6cc6b8844d40b7aa27
Max Eng <maxpersg@gmail.com>
2015-05-27 20:40:33 +0800

    bugfix

--- layer.py
+++ layer.py
@@ -79,7 +79,7 @@ class EchoLayer(YowInterfaceLayer):
         elif messagereceived == "shutdownnas":
             if recipient == authorisednumber:
 
-                os.system("ssh -i /home/pi/.ssh/id_rsa.pub root@192.168.2.50 poweroff")
+                os.system("ssh -i /home/pi/.ssh/id_rsa root@192.168.2.50 poweroff")
                 self.ReplyWith("Shutting down MAX-NAS",sendto)
 
             else:
